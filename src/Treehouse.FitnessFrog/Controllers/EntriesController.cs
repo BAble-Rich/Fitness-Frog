027e74a754aa9e3afae9aef4e3b53c0308e08504
Brent Able <Able.Brent@richlandcountysc.gov>
2025-06-17 13:36:59 -0400

    Added handler for improper values.

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -48,18 +48,22 @@ namespace Treehouse.FitnessFrog.Controllers
         We can stack the attributes, or comma separate them
         To test this we can add a breakpoint on add, then run the program*/
         [ActionName("Add"), HttpPost]
-        public ActionResult Add(string date, string activityId, string duration, string intensity, string exclude, string notes)
+        public ActionResult Add(DateTime? date, int? activityId, double? duration, Entry.IntensityLevel? intensity, bool? exclude, string notes)
         {
             /* We can also use a model to capture POST data. We wouldn't want to do it for each form field though. So MVC lets us define
               method params for each one. See Above. Also means we can rechange from AddPost to Add.
             You can now run this, and see at the breakpoint it return the values. This is Model Binding */
 
-            ViewBag.Date = date;
-            ViewBag.ActivityId = activityId;
-            ViewBag.Duration = duration;
-            ViewBag.Intensity = intensity;
-            ViewBag.Exclude = exclude;
-            ViewBag.Notes = notes;
+            /* We can also write more than strings as well through the model binder. Including enums as well. Now we may have an issue where "what if a
+             null is passed in". Well we can get around that by making these types nullable. Which is just a question mark after each type*/
+
+            /* This attempted value handles any improper value input*/
+            ViewBag.Date = ModelState["Date"].Value.AttemptedValue;
+            ViewBag.ActivityId = ModelState["ActivityId"].Value.AttemptedValue;
+            ViewBag.Duration = ModelState["Duration"].Value.AttemptedValue;
+            ViewBag.Intensity = ModelState["Intensity"].Value.AttemptedValue;
+            ViewBag.Exclude = ModelState["Exclude"].Value.AttemptedValue;
+            ViewBag.Notes = ModelState["Notes"].Value.AttemptedValue;
 
             return View();
         }
